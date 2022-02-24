bf70537de533109a11cbf47c01977d958c3a037c
ahmadsyaifulakbar24 <ipulbelcram@gmail.com>
2022-02-24 20:41:29 +0700

    add new user

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -18,9 +18,16 @@ class UserSeeder extends Seeder
     {
         User::create([
             'id' => '7bdeba5c-fc1f-4e36-9e66-f14e0519e16b',
+            'name' => 'Super Admin',
+            'email' => 'katalogumkm@kemenkopukm.go.id',
+            'password' => Hash::make('katalogumkm2022'),
+            'role' => 'super admin'
+        ]);
+        User::create([
+            'id' => '276fa62a-43de-4f38-a9c1-268472a6836f',
             'name' => 'Admin',
-            'email' => 'bti@kemenkopukm.go.id',
-            'password' => Hash::make('supporteform2022'),
+            'email' => 'adminkatalog@kemenkopukm.go.id',
+            'password' => Hash::make('adminkatalog2022'),
             'role' => 'admin'
         ]);
     }
